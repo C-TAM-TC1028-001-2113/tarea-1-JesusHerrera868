b8ed000ca0dfa7bf060d735c68241932a246f88d
Jesus Herrera 868 <A01411868@itesm.mx>
2021-08-30 16:09:48 -0500

    Acabado

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -3,10 +3,9 @@ def main():
     palabras = int(input("Dame el número de palabras: "))
     operacion1 = (palabras / 475)
     operacion2 = int(operacion1)
-    if (operacion1 - operacion2) > 0:
+    if operacion1 - operacion2 > 0:
         operacion2 = operacion2 + 1
         precio = operacion2 * 60 * 0.90
-
         print("El costo de la publicación es:", precio)
 
 if __name__ == '__main__':
