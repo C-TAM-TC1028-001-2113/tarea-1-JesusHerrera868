f4813fa130f2d8163ead64cb8c59c0e7f93a0748
Jesus Herrera 868 <A01411868@itesm.mx>
2021-08-30 16:11:49 -0500

    Acabado

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,7 +1,7 @@
 def main():
     # escribe tu código abajo de esta línea
     palabras = int(input("Dame el número de palabras: "))
-    operacion1 = (palabras / 475)
+    operacion1 = palabras / 475
     operacion2 = int(operacion1)
     if operacion1 - operacion2 > 0:
         operacion2 = operacion2 + 1
