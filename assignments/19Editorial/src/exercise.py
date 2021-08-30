11ca635ee84913aeff52c5cbddca16663f08bdaa
Jesus Herrera 868 <A01411868@itesm.mx>
2021-08-29 22:25:17 -0500

    Acabado

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -2,17 +2,12 @@ def main():
     # escribe tu código abajo de esta línea
     palabras = int(input("Dame el número de palabras: "))
     operacion1 = (palabras / 475)
-    if operacion1 > 1:
-        operacion2 = (60 * 2)
-        operacion3 = (operacion2 * 10) / 100
-        final = (operacion2 - operacion3)
-        print("El costo de la publicación es:", final)
-    else:
-        operacion4 = (60 * 1)
-        operacion5 = (operacion4 * 10) / 100
-        final2 = (operacion4 - operacion5)
-        print("El costo de la publicación es:", final2)
+    operacion2 = int(operacion1)
+    if (operacion1 - operacion2) > 0:
+        operacion2 = operacion2 + 1
+        precio = operacion2 * 60 * 0.90
 
+        print("El costo de la publicación es:", precio)
 
 if __name__ == '__main__':
     main()
