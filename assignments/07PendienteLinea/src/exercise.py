dc52cb8b47b4f1864f003d70c2b67f9597cf2c15
Jesus Herrera 868 <A01411868@itesm.mx>
2021-08-29 22:31:18 -0500

    Done

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -4,9 +4,8 @@ def main():
     y1 = float(input("Dame y1: "))
     x2 = float(input("Dame x2: "))
     y2 = float(input("Dame y2: "))
-
     m = (y2 - y1) / (x2 - x1)
-    print("Pendiente", m)
+    print("Pendiente:", m)
 
 if __name__ == '__main__':
     main()
